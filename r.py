305d7fa8083215e1da7e31d9c0a1185c369a0258
sziatnina-eng <sziatnina@gmail.com>
2025-10-16 00:48:19 +0300

    Add Movable and MovablePoint classes

---
+++ r.py
@@ -0,0 +1,27 @@
+class Movable:
+    def MoveUp(self) -> None:
+        pass
+    def MoveDown(self) -> None:
+        pass
+    def MoveLeft(self) -> None:
+        pass
+    def MoveRight(self) -> None:
+        pass
+
+class MovablePoint(Movable):
+    x: int
+    y: int
+    xSpeed: int
+    ySpeed: int
+    def __init__(self, x:int, y:int, xSpeed:int, ySpeed:int):
+        pass
+    def toString(self) -> str:
+        pass
+
+class MovableCircle:
+    center: MovablePoint
+    radius: int
+    def __init__(self, x:int, y:int, xSpeed:int, ySpeed:int, radius:int):
+        pass
+    def toString(self) -> str:
+        pass
